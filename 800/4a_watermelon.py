d895472163fa18bb94871cfae417ac07b3e196b7
Ribár Krisztián <rybarkristik@gmail.com>
2024-11-11 14:54:54 +0900

    initial commit

---
+++ 800/4a_watermelon.py
@@ -0,0 +1,12 @@
+watermelon_weight = int(input("enter watermelon weight: "))
+remaining_weight = watermelon_weight-1
+found = False;
+for i in range(2, watermelon_weight - 1):
+    remaining_weight-=1
+    if (i % 2 == 0) and (remaining_weight % 2 == 0) and (i + remaining_weight) % 2 == 0:
+        print("YES")
+        found = True
+        break
+
+if not found:
+    print("NO")
