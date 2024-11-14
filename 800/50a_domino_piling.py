e6edcbb6172cd0d2a91d7ccbdee112ff847c31a4
Ribár Krisztián <rybarkristik@gmail.com>
2024-11-14 11:50:26 +0900

    solved problem 50a

---
+++ 800/50a_domino_piling.py
@@ -0,0 +1,21 @@
+def get_domino_count(m, n):
+    if m < 0 or n < 0:
+        return 0
+
+    both_uneven = m % 2 == 1 and n % 2 == 1
+    only_m_is_even = m % 2 == 0 and n % 2 != 0
+    only_n_is_even = m % 2 != 0 and n % 2 == 0
+    one_row_or_one_line = m == 1 or n == 1
+    product_is_even = m * n % 2 == 0
+
+    if (both_uneven or one_row_or_one_line
+            or product_is_even):
+        return m * n // 2
+    elif only_m_is_even:
+        return (m * (n - 1)) // 2 + n // 2
+    elif only_n_is_even:
+        return ((m - 1) * n) // 2 + m // 2
+
+
+m, n = map(int, input().split(" "))
+print(get_domino_count(m, n))
