8c56c25d0a87d32a9f0738a32a6514511e6a4555
Ribár Krisztián <rybarkristik@gmail.com>
2024-11-17 15:29:59 +0900

    removed accidental indent from 266a

--- 800/266a_stones_on_the_table.py
+++ 800/266a_stones_on_the_table.py
@@ -1,9 +1,9 @@
-    n = int(input())
-    s = input()
+n = int(input())
+s = input()
 
-    removals = 0
-    for i in range(n - 1):
-        if s[i] == s[i + 1]:
-            removals += 1
+removals = 0
+for i in range(n - 1):
+    if s[i] == s[i + 1]:
+        removals += 1
 
-    print(removals)
+print(removals)
