d4a92283a3ab5011a6924852743d8babb41ca682
Ribár Krisztián <rybarkristik@gmail.com>
2024-12-06 23:39:40 +0900

    code cleanup

--- 800/266b_queue_at_the_school.py
+++ 800/266b_queue_at_the_school.py
@@ -1,6 +1,5 @@
 time = int(input().split()[1])
 queue = list(input())
-
 for _ in range(time):
     i = 0
     while i < len(queue) - 1:
@@ -8,5 +7,4 @@ for _ in range(time):
             queue[i], queue[i + 1] = queue[i + 1], queue[i]
             i += 1
         i += 1
-
 print("".join(queue))
--- 800/71a_way_too_long_words.py
+++ 800/71a_way_too_long_words.py
@@ -1,7 +1,7 @@
 word_count = int(input())
 for i in range(word_count):
     word = input()
-    if word.__len__() <= 10:
+    if len(word) <= 10:
         print(word)
     else:
         print(f"{word[0]}{len(word[1:-1])}{word[-1]}")
