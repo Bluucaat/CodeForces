15ab15ec0c1b0d52fb8f96af11e0e89e54fa1692
Ribár Krisztián <rybarkristik@gmail.com>
2025-03-13 00:03:35 +0100

    removed redundant code from 1154a and made the solution more optimal

--- 800/1154a_restoring_three_numbers.py
+++ 800/1154a_restoring_three_numbers.py
@@ -1,8 +1,4 @@
-sums = (list(map(int, input().split())))
-sums.sort()
-if sums[0] == sums[1] == sums[2]:
-    num = sums[3]-sums[0]
-    print(num, num, num, sep=" ")
-else:
-    print(sums[3]-sums[2], sums[3]-sums[1], sums[3]-sums[0], sep=" ")
+sums = list(map(int, input().split()))
+sum_of_all = sums.pop(sums.index(max(sums)))
+print(" ".join(str(sum_of_all - n) for n in sums))
 
